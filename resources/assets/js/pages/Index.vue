cfc0843519f6f78e848ee21bd32d1e7aa56b81df
fuadardiono <fuadardiono16@gmail.com>
2019-09-17 03:27:38 +0800

    Fix menu and build prod

--- resources/assets/js/pages/Index.vue
+++ resources/assets/js/pages/Index.vue
@@ -1,8 +1,28 @@
 <template>
+  <b-container>
+    <h3>Rekomendasi Kost</h3>
+
     <div>
-        <h2>Index</h2>
-        <router-link to="/about">To About page</router-link>
+      <b-row>
+        <b-col cols='4' v-for="obj in boardingHouse" :key="obj.id" style="margin-bottom:10px;">
+          <b-card
+            :title="obj.name"
+            :img-src="obj.image"
+            :img-alt="obj.name"
+            img-top
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              {{ obj.address }}
+            </b-card-text>
+
+            <router-link :to="{path: `/boarding-house/${obj.id}`}" class="btn btn-primary">Detail Alamat</router-link>
+          </b-card>
+        </b-col>
+      </b-row>
     </div>
+  </b-container>
 </template>
 
 <script>
